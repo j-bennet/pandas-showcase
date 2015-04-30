b5794c6e2123613b3435c7d108539f325176cb1d
Iryna Cherniavska <icherniavska@cars.com>
2015-04-30 16:52:18 -0700

    Fixing data import.

--- import_melissa_zip_code.py
+++ import_melissa_zip_code.py
@@ -4,24 +4,26 @@ import pandas as pd
 zp = pd.read_fwf(
     'data/ZIP.DAT',
     widths=[5, 2, 28, 1, 5, 7, 8, 3, 6, 1, 1, 4, 4, 3],
-    names=['zip_code', 'state', 'city', 'type', 'county_fips',
+    names=['zip_code', 'state_code', 'city_name', 'type', 'county_fips',
            'lat', 'lon', 'area_code', 'fin_code', 'last_line',
-           'facility', 'msa', 'pmsa', 'filler'],
+           'facility', 'msa_code', 'pmsa_code', 'filler'],
     usecols=[0, 1, 2, 4, 5, 6, 7, 11, 12],
     converters={'zip_code': str, 'county_fips': str, 'area_code': str,
-                'msa': str, 'pmsa': str},
+                'msa_code': str, 'pmsa_code': str},
     header=None,
     skiprows=2
 )
 
+# Filter out records without area code: FPO/APO, footer
+zp = zp[~zp.area_code.isnull()]
+
 ms = pd.read_fwf(
     'data/MSA.DAT',
     widths=[4, 4, 60, 2, 8],
-    names=['code', 'type', 'name', 'cmsa', 'population'],
+    names=['msa_code', 'type', 'msa_name', 'cmsa', 'population'],
     usecols=[0, 1, 2, 3],
-    converters={'code': str, 'type': str, 'name': str, 'cmsa': str},
-    header=None,
-    skiprows=2
+    converters={'msa_code': str, 'type': str, 'msa_name': str, 'cmsa_code': str},
+    header=None
 )
 
 # Show top records
@@ -54,24 +56,27 @@ zp.to_excel('data/zip.xlsx', sheet_name='zip_code')
 # Join zip codes on MSA, to see MSA names
 zms = zp.merge(ms, left_on="msa", right_on="code")
 
-# Select only zip code and msa code
-zip_codes = zp[["zip_code", "msa"]]
+# Select only needed cols
+zip_codes = zp[["zip_code", "state_code", "city_name", "msa_code", "area_code"]]
 
-# Rename "code" to "msa_code"
-zip_codes = zip_codes.rename(columns={'msa': 'msa_code'})
+# Rename "msa" to "msa_code"
+# zip_codes = zip_codes.rename(columns={'msa': 'msa_code'})
 
-# Select only MSA code and MSA name
-msa_codes = ms[['code', 'name']]
+# Select only needed cols
+msa_codes = ms[['msa_code', 'msa_name']]
 
 # Rename "code" to "msa_code" and "name" to "msa_name"
-msa_codes = msa_codes.rename(columns={'code': 'msa_code', 'name': 'msa_name'})
+# msa_codes = msa_codes.rename(columns={'code': 'msa_code', 'name': 'msa_name'})
 
 """
 CREATE TABLE zip_code
 (
   zip_code character varying(5) NOT NULL,
+  state_code character varying(2) NOT NULL,
+  city_name character varying(28) NOT NULL,
+  area_code character varying(3) NOT NULL,
   msa_code character(4),
-  CONSTRAINT zip_code_pkey PRIMARY KEY (zip_code)
+  CONSTRAINT zip_code_pkey PRIMARY KEY (zip_code, state_code, city_name)
 )
 WITH (
   OIDS=FALSE
@@ -90,7 +95,7 @@ WITH (
 
 from sqlalchemy import create_engine
 
-engine = create_engine('postgresql://username:password@localhost:5432/playground')
+engine = create_engine('postgresql://username:pass@localhost:5432/playground')
 
-zip_codes.to_sql('zip_code', engine)
-msa_codes.to_sql('msa', engine)
+zip_codes.to_sql('zip_code', engine, index=False, if_exists='append')
+msa_codes.to_sql('msa', engine, index=False, if_exists='append')
