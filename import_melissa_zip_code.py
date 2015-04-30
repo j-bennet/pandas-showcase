cf55f76bdfa6efe031c9752161c0ce63e1af89a4
Iryna Cherniavska <icherniavska@cars.com>
2015-04-30 16:08:54 -0700

    Loading Melissa zip code data.

---
+++ import_melissa_zip_code.py
@@ -0,0 +1,96 @@
+import pandas as pd
+
+# Read a fixed-width file
+zp = pd.read_fwf(
+    'data/ZIP.DAT',
+    widths=[5, 2, 28, 1, 5, 7, 8, 3, 6, 1, 1, 4, 4, 3],
+    names=['zip_code', 'state', 'city', 'type', 'county_fips',
+           'lat', 'lon', 'area_code', 'fin_code', 'last_line',
+           'facility', 'msa', 'pmsa', 'filler'],
+    usecols=[0, 1, 2, 4, 5, 6, 7, 11, 12],
+    converters={'zip_code': str, 'county_fips': str, 'area_code': str,
+                'msa': str, 'pmsa': str},
+    header=None,
+    skiprows=2
+)
+
+ms = pd.read_fwf(
+    'data/MSA.DAT',
+    widths=[4, 4, 60, 2, 8],
+    names=['code', 'type', 'name', 'cmsa', 'population'],
+    usecols=[0, 1, 2, 3],
+    converters={'code': str, 'type': str, 'name': str, 'cmsa': str},
+    header=None,
+    skiprows=2
+)
+
+# Show top records
+print zp.head(10)
+
+# Sort
+print zp.sort(columns=['zip_code']).head(10)
+
+# Select subset of cols
+print zp['zip_code']
+
+# Select subset of rows
+print zp[0:3]
+
+# Select just one cols
+print zp.loc[0:3, ['zip_code']]
+
+# Select rows on condition
+print zp[zp.zip_code == '90703']
+
+# Select on "IN" condition
+print zp[zp.zip_code.isin(['90401', '90703'])]
+
+# Select all rows that have missing values (for sanity check)
+print zp[zp.isnull().any(axis=1)]
+
+# Save it as Excel: needs openpyxl==1.8.6 installed
+zp.to_excel('data/zip.xlsx', sheet_name='zip_code')
+
+# Join zip codes on MSA, to see MSA names
+zms = zp.merge(ms, left_on="msa", right_on="code")
+
+# Select only zip code and msa code
+zip_codes = zp[["zip_code", "msa"]]
+
+# Rename "code" to "msa_code"
+zip_codes = zip_codes.rename(columns={'msa': 'msa_code'})
+
+# Select only MSA code and MSA name
+msa_codes = ms[['code', 'name']]
+
+# Rename "code" to "msa_code" and "name" to "msa_name"
+msa_codes = msa_codes.rename(columns={'code': 'msa_code', 'name': 'msa_name'})
+
+"""
+CREATE TABLE zip_code
+(
+  zip_code character varying(5) NOT NULL,
+  msa_code character(4),
+  CONSTRAINT zip_code_pkey PRIMARY KEY (zip_code)
+)
+WITH (
+  OIDS=FALSE
+);
+
+CREATE TABLE msa
+(
+  msa_code character(4) not null,
+  msa_name character(60) not null,
+  CONSTRAINT msa_pkey PRIMARY KEY (msa_code)
+)
+WITH (
+  OIDS=FALSE
+);
+"""
+
+from sqlalchemy import create_engine
+
+engine = create_engine('postgresql://username:password@localhost:5432/playground')
+
+zip_codes.to_sql('zip_code', engine)
+msa_codes.to_sql('msa', engine)
